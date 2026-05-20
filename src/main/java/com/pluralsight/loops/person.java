ce7d65c9d3013a5c39098cb182eb87a365718dfc
edomg <edomgezahegn54@gmail.com>
2026-05-20 15:02:46 -0500

    Add Person class with first name last name and age fields

--- src/main/java/com/pluralsight/loops/person.java
+++ src/main/java/com/pluralsight/loops/person.java
@@ -1,4 +1,13 @@
 package com.pluralsight.loops;
 
 public class person {
+    String firstName;
+    String lastName;
+    int age;
+
+    public person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
 }
